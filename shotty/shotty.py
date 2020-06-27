ed05a7e19eed660b6b521ba0ff8130f058725202
Paul Widmer <pwidmer@gmail.com>
2020-06-27 15:09:34 -0400

    Stop and start instances and wait appropriately durrint the snapshot process

--- shotty/shotty.py
+++ shotty/shotty.py
@@ -84,9 +84,24 @@ def create_snapshots(project):
     instances = filter_instances(project)
 
     for i in instances:
+
+        print("Stopping {0}...".format(i.id))
+        
+        i.stop()
+        #use EC2 waiter, will poll every 15 seconds until stopped
+        i.wait_until_stopped()
+        
         for v in i.volumes.all():
-            print("Creating snapshot of {0}".format(v.id))
+            print("   Creating snapshot of {0}".format(v.id))
             v.create_snapshot(Description="Created by Snapshotalyzer")
+        
+        print("Starting {0}...".format(i.id))
+        i.start()
+        #assuming that it is ideal to do one snapshot at a time to minimize
+        #downtime impact of servers, wait until running before going on to next
+        i.wait_until_running()
+
+    print("Job done")
 
     return
 
